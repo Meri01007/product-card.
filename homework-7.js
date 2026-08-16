ac083799a4174bd46a6a3107f1d36c2831bd48e0
Meretguly <bely0102@gmail.com>
2026-08-16 19:31:13 +0500

    feat: add product card

--- homework-7.js
+++ homework-7.js
@@ -1,9 +1,9 @@
 function forecast(city, temperature){
-  console.log( `Сейчас в ${city} температура - ${temperature} градусов по Цельсию`)
+  console.log( `Сейчас в ${city} температура - ${temperature} градусов по Цельсию`);
 }
-forecast('Гумдаг', 55);
+forecast('Гумдаг', 55); 
 
-const LIGHT_SPEED = 299792458
+const LIGHT_SPEED = 299792458;
 
 function checkSpeed (highSpeed){
   if ( highSpeed>LIGHT_SPEED ){
@@ -13,30 +13,30 @@ function checkSpeed (highSpeed){
    else (LIGHT_SPEED===LIGHT_SPEED) 
     console.log('Скорость света');}
 
-    checkSpeed(299792459)
-    checkSpeed(299792457)
+    checkSpeed(299792459);
+    checkSpeed(299792457);
 
-    const car = 'Toyota Camry'
-    const carPrice = 2500
+    const car = 'Toyota Camry';
+    const carPrice = 2500;
 
     function calculateBudget(money){
       if (money>=carPrice){
-        console.log(`${car} приобретён. Спасибо за покупку!`)
+        console.log(`${car} приобретён. Спасибо за покупку!`);
       } else
       {const difference = carPrice - money;{
-          console.log(`Вам не хватает ${difference}, Пополните баланс`)
+          console.log(`Вам не хватает ${difference}, Пополните баланс`);
         }}
 
     }
-    calculateBudget(3000)
-    calculateBudget(2000)
+    calculateBudget(3000);
+    calculateBudget(2000);
 
 
 function showMessage(){
-  console.log('Функция создана для улучшения аналитических способностей дз7')
+  console.log('Функция создана для улучшения аналитических способностей дз7');
 }
-showMessage()
+showMessage();
 
-var meri = 'учебная переменная вар'
-let meri01 = 'учебная переменная лет'
-const meri02 = 'учебная переменная консты'
+var meri = 'учебная переменная вар';
+let meri01 = 'учебная переменная лет';
+const meri02 = 'учебная переменная консты';
