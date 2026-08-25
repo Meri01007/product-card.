438c0d891a61b49dd0775657f03ede946343748c
Meretguly <bely0102@gmail.com>
2026-08-25 16:22:31 +0500

    feat: add product card

--- homework-8.js
+++ homework-8.js
@@ -41,40 +41,14 @@ function checkValueObject(obj, key) {
   }
 }
 
-const products = [
-  "Увлажняющий мусс",
-  "Увлажняющая маска",
-  "Гель для умывания",
-  "Подарочный набор №1",
-  "Подарочный набор №5",
-];
+const products = [ "Увлажняющий мусс", "Увлажняющая маска", "Гель для умывания", "Подарочный набор №1", "Подарочный набор №5",];
+
 console.log(products);
 
-const theWire = {
-  title: "Прослушка",
-  rate: 9.3,
-  country: "США",
-  year: 1999,
-  genre: "Криминал, Триллер, Драма",
-};
-
-const mutiny = {
-  title: "Мятеж",
-  rate: 0.0,
-  country: "Великобритания",
-  year: 2026,
-  genre: "Криминал, Триллер, Боевик",
-};
-
-const youngWashington = {
-  title: "Молодой Вашингтон",
-  rate: 7.5,
-  country: "США",
-  year: 2026,
-  genre: "Биография, История",
-};
-
-const movies = [theWire, mutiny, youngWashington];
+const movies = [ {title: "Прослушка", rate: 9.3, country: "США", year: 1999, genre: "Криминал, Триллер, Драма"},
+  {title: "Мятеж", rate: 0.0, country: "Великобритания", year: 2026, genre: "Криминал, Триллер, Боевик"},
+  {title: "Молодой Вашингтон", rate: 7.5, country: "США", year: 2026, genre: "Биография, История"}
+];
 
 const luckyStrike = {
   title: "Последний рубеж",
@@ -88,42 +62,19 @@ movies.push(luckyStrike);
 
 console.log(movies);
 
-const walle = {
-  title: "Валли",
-  rate: 8.4,
-  country: "США",
-  year: 2008,
-  genre: "Приключения, Семейный, Фантастика",
-};
+const cartoons = [ {title: "Валли", rate: 8.4, country: "США", year: 2008, genre: "Приключения, Семейный, Фантастика"},
+{ title: "Рататуй", rate: 8.1, country: "США", year: 2007, genre: "Комедия, Приключения, Семейный, Фэнтези, Драма"},
+{ title: "Ральф", rate: 7.7, country: "США", year: 2012, genre: "Комедия, Детские, Приключения, Семейный, Фэнтези"}]
 
-const ratatouille = {
-  title: "Рататуй",
-  rate: 8.1,
-  country: "США",
-  year: 2007,
-  genre: "Комедия, Приключения, Семейный, Фэнтези, Драма",
-};
-
-const wreckItRalph = {
-  title: "Ральф",
-  rate: 7.7,
-  country: "США",
-  year: 2012,
-  genre: "Комедия, Детские, Приключения, Семейный, Фэнтези",
-};
-
-const cartoons = [walle, ratatouille, wreckItRalph];
 console.log(cartoons);
 
 const allmovies = [...cartoons, ...movies];
 console.log(allmovies);
 
-function isRare(cinema) {
-  if (cinema.year > 2000) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
+const checkMovieYear = allmovies.map((movie) => {
+  return {
+    isRare: movie.year > 2000
+  };
+});
 
-isRare(allmovies);
+console.log(checkMovieYear);
