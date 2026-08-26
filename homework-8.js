cc5a62d6e14598d3b7943192ba8a27a512406dc9
Meretguly <bely0102@gmail.com>
2026-08-27 00:42:34 +0500

    feat: add product card

--- homework-8.js
+++ homework-8.js
@@ -27,7 +27,7 @@ function checkSpeed(car) {
   else {
     car.maxspeed = 150;
   }
-};
+}
 
 checkSpeed(carDetailes);
 console.log(carDetailes.maxspeed);
@@ -39,34 +39,40 @@ function checkValueObject(obj, key) {
   if (key in obj) {
     console.log(obj[key]);
   }
-};
+}
 
-const products = [ "Увлажняющий мусс", "Увлажняющая маска", "Гель для умывания", "Подарочный набор №1", "Подарочный набор №5",];
+const products = [
+  "Увлажняющий мусс",
+  "Увлажняющая маска",
+  "Гель для умывания",
+  "Подарочный набор №1",
+  "Подарочный набор №5",
+];
 
 console.log(products);
 
-const movies = [ 
-{
-  title: "Прослушка",
-  rate: 9.3,
-  country: "США",
-  year: 1999,
-  genre: "Криминал, Триллер, Драма"
-},
-{
-  title: "Мятеж",
-  rate: 0.0,
-  country: "Великобритания",
-  year: 2026,
-  genre: "Криминал, Триллер, Боевик"
-},
-{
-  title: "Молодой Вашингтон",
-  rate: 7.5,
-  country: "США",
-  year: 2026,
-  genre: "Биография, История"
-}
+const movies = [
+  {
+    title: "Прослушка",
+    rate: 9.3,
+    country: "США",
+    year: 1999,
+    genre: "Криминал, Триллер, Драма",
+  },
+  {
+    title: "Мятеж",
+    rate: 0.0,
+    country: "Великобритания",
+    year: 2026,
+    genre: "Криминал, Триллер, Боевик",
+  },
+  {
+    title: "Молодой Вашингтон",
+    rate: 7.5,
+    country: "США",
+    year: 2026,
+    genre: "Биография, История",
+  },
 ];
 
 const luckyStrike = {
@@ -81,28 +87,28 @@ movies.push(luckyStrike);
 
 console.log(movies);
 
-const cartoons = [ 
-{
-  title: "Валли",
-  rate: 8.4,
-  country: "США",
-  year: 2008,
-  genre: "Приключения, Семейный, Фантастика"
-},
-{
-  title: "Рататуй",
-  rate: 8.1,
-  country: "США",
-  year: 2007,
-  genre: "Комедия, Приключения, Семейный, Фэнтези, Драма"
-},
-{
-  title: "Ральф",
-  rate: 7.7,
-  country: "США",
-  year: 2012,
-  genre: "Комедия, Детские, Приключения, Семейный, Фэнтези"
-}
+const cartoons = [
+  {
+    title: "Валли",
+    rate: 8.4,
+    country: "США",
+    year: 2008,
+    genre: "Приключения, Семейный, Фантастика",
+  },
+  {
+    title: "Рататуй",
+    rate: 8.1,
+    country: "США",
+    year: 2007,
+    genre: "Комедия, Приключения, Семейный, Фэнтези, Драма",
+  },
+  {
+    title: "Ральф",
+    rate: 7.7,
+    country: "США",
+    year: 2012,
+    genre: "Комедия, Детские, Приключения, Семейный, Фэнтези",
+  },
 ];
 
 console.log(cartoons);
@@ -112,7 +118,7 @@ console.log(allmovies);
 
 const checkMovieYear = allmovies.map((movie) => {
   return {
-    isRare: movie.year > 2000
+    isRare: movie.year > 2000,
   };
 });
 
